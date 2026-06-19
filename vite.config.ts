630bdde35188336149aa87f84b5f82f7605899c9
Ricardo Barros <petericardo03@gmail.com>
2026-06-19 14:39:56 +0100

    fix: fix github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2020',
+    target: "es2020",
     chunkSizeWarningLimit: 800,
   },
+  base: "/my-resume/",
 });
